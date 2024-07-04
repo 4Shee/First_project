05cf45dd08a133b0165cd9d7aca6a694fb9b55fc
Paul Moskalev <jessylaiho@gmail.com>
2024-07-04 19:45:59 +0400

    complete second section

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 "use strict";
 
 const numberOfFilms = prompt("How many films you've seen already?");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+}
