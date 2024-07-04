6bd6cd9c624249b58b04bc19c4b20ee3db9db81d
Paul Moskalev <jessylaiho@gmail.com>
2024-07-04 19:58:18 +0400

    complete third section

--- scripts/script.js
+++ scripts/script.js
@@ -8,4 +8,12 @@ const personalMovieDB = {
   actors: {},
   genres: [],
   private: false,
-}
+};
+
+
+let lastFilmName = prompt("One of the last film what you have seen?");
+const ratingOfFilm = prompt("Wich rating you want to give it?");
+
+personalMovieDB.movies[`${lastFilmName}`] = ratingOfFilm;
+
+console.log(personalMovieDB);
