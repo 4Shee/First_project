92b48c0f25daed8b583b4713df92e8ff1cfe3499
Paul Moskalev <jessylaiho@gmail.com>
2024-07-04 23:32:21 +0400

    Really final hatch

--- scripts/script.js
+++ scripts/script.js
@@ -14,11 +14,11 @@ const personalMovieDB = {
 let lastFilmName = prompt("One of the last film what you have seen?", '');
 let ratingOfFilm = prompt("Wich rating you want to give it?", '');
 
-personalMovieDB.movies[`${lastFilmName}`] = ratingOfFilm;
+personalMovieDB.movies[lastFilmName] = ratingOfFilm;
 
 lastFilmName = prompt("One of the last film what you have seen?", '');
 ratingOfFilm = prompt("Wich rating you want to give it?", '');
 
-personalMovieDB.movies[`${lastFilmName}`] = ratingOfFilm;
+personalMovieDB.movies[lastFilmName] = ratingOfFilm;
 
 console.log(personalMovieDB);
