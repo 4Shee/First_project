79cff2afd968271311ad5ab196fe982f9f722c97
Paul Moskalev <jessylaiho@gmail.com>
2024-07-04 23:30:03 +0400

    Final hatch

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = prompt("How many films you've seen already?");
+const numberOfFilms = prompt("How many films you've seen already?", '');
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -11,8 +11,13 @@ const personalMovieDB = {
 };
 
 
-let lastFilmName = prompt("One of the last film what you have seen?");
-const ratingOfFilm = prompt("Wich rating you want to give it?");
+let lastFilmName = prompt("One of the last film what you have seen?", '');
+let ratingOfFilm = prompt("Wich rating you want to give it?", '');
+
+personalMovieDB.movies[`${lastFilmName}`] = ratingOfFilm;
+
+lastFilmName = prompt("One of the last film what you have seen?", '');
+ratingOfFilm = prompt("Wich rating you want to give it?", '');
 
 personalMovieDB.movies[`${lastFilmName}`] = ratingOfFilm;
 
