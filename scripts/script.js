8403978a79b93541abe586b5adf7cc050f6f449b
Paul Moskalev <jessylaiho@gmail.com>
2024-07-06 21:58:07 +0400

    Now all functions are method's of presonalMovieDB

--- scripts/script.js
+++ scripts/script.js
@@ -1,85 +1,80 @@
 "use strict";
 
-const start = function () {
-  for (; ;) {
-    const numberOfFilms = +prompt("How many films you've seen already?", '');
-    if (checkConditions(numberOfFilms) || isNaN(numberOfFilms)) {
-      continue;
-    }
-    return numberOfFilms;
-  }
-};
-
-function checkConditions(value) {
-  if (value == null
-    || value == ''
-    || value.length > 50
-  ) {
-    return true
-  }
-  return false
-}
-
-const showMyDB = function (obj) {
-  if (!obj.private) {
-    return console.log(obj);
-  }
-  return console.log('This is a private object');
-};
-
-const writeYourGenres = function (obj) {
-  for (let i = 0; i < 3; i++) {
-    obj.genres[i] = prompt(`Your ${i + 1} favorite genre?`)
-  }
-};
-
-const compareFilmsNumber = function (obj) {
-  if (obj.count < 10) {
-    alert("You have watched a few films");
-  } else if (obj.count > 10 && obj.count < 30) {
-    alert("You are a common viewer");
-  } else if (obj.count > 30) {
-    alert("You are a cinephile");
-  }
-};
-
-const getFilmName = function () {
-  for (; ;) {
-    const lastFilmName = prompt("One of the last film what you have seen?", '');
-    if (checkConditions(lastFilmName)) {
-      continue;
-    } return lastFilmName;
-  }
-};
-
-const getFilmRating = function () {
-  while (true) {
-    const ratingOfFilm = prompt("Wich rating you want to give it?", '');
-    if (checkConditions(ratingOfFilm)) {
-      continue;
-    } return ratingOfFilm;
-  }
-};
-
-const addFilmAndRating = function (obj) {
-  do {
-    obj.movies[getFilmName()] = getFilmRating();
-  }
-  while (prompt('Want to rate another film?(y-yes)', '').toLowerCase() === 'y')
-};
-
 const personalMovieDB = {
-  count: start(),
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   private: false,
+  start: function () {
+    for (; ;) {
+      const numberOfFilms = +prompt("How many films you've seen already?", '');
+      if (this.checkConditions(numberOfFilms) || isNaN(numberOfFilms)) {
+        continue;
+      }
+      this.count = numberOfFilms;
+      break;
+    }
+  },
+  checkConditions: function (value) {
+    if (value == null
+      || value == ''
+      || value.length > 50
+    ) {
+      return true
+    }
+    return false
+  },
+  showMyDB: function () {
+    if (!this.private) {
+      return console.log(this);
+    }
+    return console.log('This is a private object');
+  },
+  compareFilmsNumber: function () {
+    if (this.count < 10) {
+      alert("You have watched a few films");
+    } else if (this.count > 10 && this.count < 30) {
+      alert("You are a common viewer");
+    } else if (this.count > 30) {
+      alert("You are a cinephile");
+    }
+  },
+  writeYourGenres: function () {
+    for (let i = 0; i < 3; i++) {
+      this.genres[i] = prompt(`Your ${i + 1} favorite genre?`)
+    }
+  },
+  addFilmAndRating: function () {
+    do {
+      this.movies[this.getFilmName()] = this.getFilmRating();
+    }
+    while (prompt('Want to rate another film?(y-yes)', '').toLowerCase() === 'y')
+  },
+  getFilmName: function () {
+    for (; ;) {
+      const lastFilmName = prompt("One of the last film what you have seen?", '');
+      if (this.checkConditions(lastFilmName)) {
+        continue;
+      } return lastFilmName;
+    }
+  },
+  getFilmRating: function () {
+    while (true) {
+      const ratingOfFilm = prompt("Wich rating you want to give it?", '');
+      if (this.checkConditions(ratingOfFilm)) {
+        continue;
+      } return ratingOfFilm;
+    }
+  },
 };
 
-compareFilmsNumber(personalMovieDB);
+personalMovieDB.start();
 
-writeYourGenres(personalMovieDB);
+personalMovieDB.compareFilmsNumber();
 
-addFilmAndRating(personalMovieDB);
+personalMovieDB.writeYourGenres();
 
-showMyDB(personalMovieDB);
+personalMovieDB.addFilmAndRating();
+
+personalMovieDB.showMyDB();
